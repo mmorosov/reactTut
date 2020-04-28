498ce9f82409d59d39d3720c7ec5cb208412d432
Mariya Morosovska <mariyamorosov@gmail.com>
2020-04-28 10:55:37 -0400

    Updates

---
+++ precache-manifest.9f98370dbd44c0d63667e3f14841e069.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e229eef9a01ba164e75553a6927edd9d",
+    "url": "/reactTut/index.html"
+  },
+  {
+    "revision": "865240159c80732c725f",
+    "url": "/reactTut/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "fe7a590503a38c731f23",
+    "url": "/reactTut/static/js/2.6f634fff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactTut/static/js/2.6f634fff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "865240159c80732c725f",
+    "url": "/reactTut/static/js/main.8bfb4538.chunk.js"
+  },
+  {
+    "revision": "af187eb4b50381a837dd",
+    "url": "/reactTut/static/js/runtime-main.7c6a23a2.js"
+  }
+]);
